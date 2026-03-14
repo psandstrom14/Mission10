adf2cd9bf6cc2560c65bb4376b1ac91a39ef463e
psandstrom14 <psandstrom8@gmail.com>
2026-03-14 09:09:20 -0600

    finished the backend

---
+++ backend/Mission10/Mission10/Controllers/BowlersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mission10.Data; // This imports your scaffolded context and models
+
+namespace Mission10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BowlersController : ControllerBase
+    {
+        private readonly BowlingLeagueContext _context;
+
+        public BowlersController(BowlingLeagueContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<object>>> GetMarlinsAndSharks()
+        {
+            // We join the Teams table, filter for Marlins/Sharks, and select the exact fields required
+            var bowlers = await _context.Bowlers
+                .Include(b => b.Team)
+                .Where(b => b.Team.TeamName == "Marlins" || b.Team.TeamName == "Sharks")
+                .Select(b => new
+                {
+                    BowlerId = b.BowlerId,
+                    FirstName = b.BowlerFirstName,
+                    MiddleInit = b.BowlerMiddleInit,
+                    LastName = b.BowlerLastName,
+                    TeamName = b.Team.TeamName,
+                    Address = b.BowlerAddress,
+                    City = b.BowlerCity,
+                    State = b.BowlerState,
+                    Zip = b.BowlerZip,
+                    PhoneNumber = b.BowlerPhoneNumber
+                })
+                .ToListAsync();
+
+            return Ok(bowlers);
+        }
+    }
+}
--- backend/Mission10/Mission10/Program.cs
+++ backend/Mission10/Mission10/Program.cs
@@ -1,20 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using Mission10.Data; 
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<BowlingLeagueContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("BowlingConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        corsBuilder => 
+        {
+            corsBuilder.WithOrigins("http://localhost:5173", "http://localhost:3000") // standard Vite and React ports
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
